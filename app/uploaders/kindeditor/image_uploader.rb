8e1a27dfbc23b5e3e7d0b4e73342d0013062d13d
Macrow <Macrow_wh@163.com>
2011-06-12 12:19:17 +0800

    v0.0.2 patch

--- app/uploaders/kindeditor/image_uploader.rb
+++ app/uploaders/kindeditor/image_uploader.rb
@@ -16,9 +16,9 @@ class Kindeditor::ImageUploader < CarrierWave::Uploader::Base
     "uploads/Image/#{Time.now.strftime("%Y-%m")}"
   end
 
-  #def cache_dir
-  #  "#{Rails.root}/tmp/uploads"
-  #end
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -45,8 +45,23 @@ class Kindeditor::ImageUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  before :store, :remember_cache_id
+  after :store, :delete_tmp_dir
+
+  # store! nil's the cache_id after it finishes so we need to remember it for deletition
+  def remember_cache_id(new_file)
+    @cache_id_was = cache_id
+  end
+
+  def delete_tmp_dir(new_file)
+    # make sure we don't delete other things accidentally by checking the name pattern
+    if @cache_id_was.present? && @cache_id_was =~ /\A[\d]{8}\-[\d]{4}\-[\d]+\-[\d]{4}\z/
+      FileUtils.rm_rf(File.join(cache_dir, @cache_id_was))
+    end
+  end
+
   def filename
-    @name ||= Time.now.to_date.to_s(:db) + "-" + ActiveSupport::SecureRandom.hex
+    @name ||= Time.now.to_s(:number)
     "#{@name}#{File.extname(original_filename).downcase}" if original_filename
   end
 
