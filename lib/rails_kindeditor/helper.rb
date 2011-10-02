946117458441ef3586ded6a6cc460f56926c5c88
Macrow <Macrow_wh@163.com>
2011-10-02 13:08:31 +0800

    fixed bug under sprockets2.0.1

--- lib/rails_kindeditor/helper.rb
+++ lib/rails_kindeditor/helper.rb
@@ -6,7 +6,7 @@ module RailsKindeditor
         if ::Rails.version < "3.1.0"
           path = "/javascripts"
         else # Rails3.1+
-          path = ""
+          path = "#{request.protocol}#{request.host_with_port}"
         end
         if config.perform_caching && ::Rails.version < "3.1.0"
           output << javascript_include_tag("#{path}/kindeditor/kindeditor-min.js", "#{path}/kindeditor/kindeditor-init.js", :cache => "kindeditor/kindeditor-cache")
