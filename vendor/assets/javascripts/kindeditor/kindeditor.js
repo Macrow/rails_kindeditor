f1a5ce435a72dc22db9066112609ed3370896f14
backer <zm.backer@gmail.com>
2015-01-18 11:37:04 +0800

    恢复 _getBasePath

--- vendor/assets/javascripts/kindeditor/kindeditor.js
+++ vendor/assets/javascripts/kindeditor/kindeditor.js
@@ -217,14 +217,13 @@ var _INLINE_TAG_MAP = _toMap('a,abbr,acronym,b,basefont,bdo,big,br,button,cite,c
 // Begining of modification by Macrow
 function _getBasePath() {
 	var refPath = '/assets/kindeditor/';
-  // var els = document.getElementsByTagName('script'), src;
-  // for (var i = 0, len = els.length; i < len; i++) {
-  //   src = els[i].src || '';
-  //   if (/(kindeditor|application)[\w\-\.]*\.js/.test(src)) {
-  //     return src.substring(0, src.indexOf('assets')) + refPath;
-  //   }
-  // }
-	refPath = 'http://asset1.wityun.com/damitech/kindeditor/'
+	var els = document.getElementsByTagName('script'), src;
+	for (var i = 0, len = els.length; i < len; i++) {
+		src = els[i].src || '';
+		if (/(kindeditor|application)[\w\-\.]*\.js/.test(src)) {
+			return src.substring(0, src.indexOf('assets')) + refPath;
+		}
+	}
 	return refPath;
 }
 // End of modification by Macrow
