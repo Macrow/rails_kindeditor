4959eca457dcef299a1c99991054de01b9b7b90c
mike <mike@beansmile.com>
2016-09-22 14:22:35 +0800

    fix DEPRECATION WARNING: input method now accepts a `wrapper_options` argument.

--- lib/rails_kindeditor/simple_form.rb
+++ lib/rails_kindeditor/simple_form.rb
@@ -1,7 +1,7 @@
 module RailsKindeditor
   module SimpleForm
     class KindeditorInput < ::SimpleForm::Inputs::Base
-      def input
+      def input(wrapper_options)
         @builder.kindeditor(attribute_name, input_html_options)
       end
     end
