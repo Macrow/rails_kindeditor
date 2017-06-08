400aed03871af77c9edc87fcfa04665ae09c4964
wzxia <280008163@qq.com>
2017-06-08 09:41:26 +0800

    modify video

--- vendor/assets/javascripts/kindeditor/kindeditor.js
+++ vendor/assets/javascripts/kindeditor/kindeditor.js
@@ -950,16 +950,13 @@ function _mediaAttrs(srcTag) {
 }
 function _mediaEmbed(attrs) {
 	var html = '';
-    if (/\.(mp4|ogg|mp3|webm)(\?|$)/i.test(attrs.flashvars)) {
-        html += "<video src='"+ attrs.flashvars.substr(5,attrs.flashvars.length) +"'  width='"+ attrs.width + "' height='"+ attrs.height +"' controls='controls'>";
-    }
     html += '<embed name="player" allowscriptaccess="always" allowfullscreen="true" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" ';
-	_each(attrs, function(key, val) {
-		html += key + '="' + val + '" ';
-	});
-	html += '/>';
-    if (/\.(mp4|ogg|mp3|webm)(\?|$)/i.test(attrs.flashvars)) {
-        html += '</video>';
+    _each(attrs, function(key, val) {
+        html += key + '="' + val + '" ';
+    });
+    html += '/>';
+    if (/\.(mp4|ogg|mp3|webm|f4v)(\?|$)/i.test(attrs.flashvars)) {
+        html = "<video src='"+ attrs.flashvars.substr(5,attrs.flashvars.length) +"'  width='"+ attrs.width + "' height='"+ attrs.height +"' controls='controls'>" + html + '</video>'
     }
 	return html;
 }
