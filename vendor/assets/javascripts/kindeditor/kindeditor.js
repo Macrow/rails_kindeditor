0c49cd575c735ac02894158cc4438115c4cfff99
wzxia <280008163@qq.com>
2017-07-20 16:53:12 +0800

    modify video

--- vendor/assets/javascripts/kindeditor/kindeditor.js
+++ vendor/assets/javascripts/kindeditor/kindeditor.js
@@ -236,7 +236,7 @@ K.basePath = _getBasePath();
 K.options = {
 	designMode : true,
 	fullscreenMode : false,
-	filterMode : true,
+	filterMode : false,
 	wellFormatMode : true,
 	shadowMode : true,
 	loadStyleMode : true,
